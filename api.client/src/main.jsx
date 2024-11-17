8065cdb9e49e79f0b207991e630ddb86f32dde3c
Rafal Labecki <ralfilab@hotmail.co.uk>
2024-11-17 18:46:37 +0100

    Remove react router warning messages about transition to v7

--- api.client/src/main.jsx
+++ api.client/src/main.jsx
@@ -26,7 +26,16 @@ const router = createBrowserRouter([
       },
     ],
   },
-]);
+], {
+  future: {
+    v7_startTransition: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,    
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
